c1a5ddc6adbefc7890a89f4532a6c7d8430a6f4c
Jonkovic <38046679+Jonkovic@users.noreply.github.com>
2018-05-07 01:18:15 -0500

    Update main.py
    Added auto save and manual load, as well as print to csv file capability

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
+import sqlite3
+import csv
 
 
+
+
+
+conn = sqlite3.connect('Participants.db')
+c = conn.cursor()
+def create_table():
+    c.execute("CREATE TABLE if not exists example(Student_Name)")
+    conn.commit()
+create_table()
 class StudentListButton(ListItemButton):
     pass
 
@@ -29,6 +40,9 @@ class StudentDB(BoxLayout):
  
         # Reset the ListView
         self.student_list._trigger_reset_populate()
+
+        c.execute("INSERT INTO example (Student_Name) VALUES (?)", (student_name,))
+        conn.commit()
 
     def spinner_clicked(self, value):
         print("Spinner Value " + value)
@@ -57,6 +71,16 @@ class StudentDB(BoxLayout):
  
             # Reset the ListView
             self.student_list._trigger_reset_populate()
+
+            print(selection + " Will be deleted")
+
+            conn.execute("DELETE FROM example WHERE (Student_Name) = (?)", (selection,))
+            conn.commit()
+
+            
+
+
+
  
     def replace_student(self, *args):
  
@@ -68,7 +92,12 @@ class StudentDB(BoxLayout):
  
             # Remove the matching item
             self.student_list.adapter.data.remove(selection)
- 
+
+            print(selection + " Will be Replaced")
+
+            conn.execute("DELETE FROM example WHERE (Student_Name) = (?)", (selection,))
+            conn.commit()
+
             # Get the student name from the TextInputs
             student_name = (self.last_name_text_input.text
                             + ", " + self.first_name_text_input.text
@@ -77,10 +106,36 @@ class StudentDB(BoxLayout):
  
             # Add the updated data to the list
             self.student_list.adapter.data.extend([student_name])
- 
+
+            print(selection + " Will be Replaced By "+student_name)
+
+            c.execute("INSERT INTO example (Student_Name) VALUES (?)", (student_name,))
+            conn.commit()
+
             # Reset the ListView
             self.student_list._trigger_reset_populate()
- 
+    def load_students(self, *args):
+        sql = "SELECT * FROM example"
+        for row in c.execute(sql):
+            print(row)
+            student_name = (row[0])
+            self.student_list.adapter.data.extend([student_name])
+
+            self.student_list._trigger_reset_populate()
+    def print_csv(self, *args):
+        sql = "SELECT * FROM example"
+        with open('Participants.csv', 'w') as file:
+            csv_writer = csv.writer(file)
+            for row in c.execute(sql):
+                csv_writer.writerow(row)
+                
+
+
+
+
+
+
+
  
 class StudentDBApp(App):
     def build(self):
@@ -88,3 +143,5 @@ class StudentDBApp(App):
  
  
 dbApp = StudentDBApp()
+
+dbApp.run()
